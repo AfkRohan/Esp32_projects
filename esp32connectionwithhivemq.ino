76775a19c9c68540d4564fd1dc84994cee6d9496
Rohan Shah <70683368+AfkRohan@users.noreply.github.com>
2022-04-19 11:17:18 +0530

    Add files via upload

---
+++ esp32connectionwithhivemq.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "ADVANCETECH_DIR_4G";
+const char* password =  "12761487";
+const char* mqttServer = "broker.mqttdashboard.com";
+const int mqttPort = 1883;
+const char* mqttUser = "rohan";
+const char* mqttPassword = "rohan";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.subscribe("esp/test");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
